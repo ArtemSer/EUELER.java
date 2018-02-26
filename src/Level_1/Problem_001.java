91248b12bb66472d4203548c8420bce6526896fa
ArtemSer <artem.sierikov@gmail.com>
2018-02-26 19:02:58 +0200

    Problem_001 Status: Done
    Answer: 233168

--- src/Level_1/Problem_001.java
+++ src/Level_1/Problem_001.java
@@ -1,10 +1,17 @@
 package Level_1;
 /*
+ * Description: If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ *              we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Mission:     Find the sum of all the multiples of 3 or 5 below 1000.
  *
- *
+ * Author:      Sierikov Artem  ()
  */
 public class Problem_001 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) result += i;
+        }
+        System.out.println("Result is " + result);
     }
 }
