2915aa57b157999819da0e15d69ab029b0c2499f
ArtemSer <artem.sierikov@gmail.com>
2018-02-26 20:03:07 +0200

    Problem_003 Status: Done
    Answer: 6857

--- src/Level_1/Problem_003.java
+++ src/Level_1/Problem_003.java
@@ -1,4 +1,31 @@
 package Level_1;
+/*
+ * Description: The prime factors of 13195 are 5, 7, 13 and 29.
+ * Mission:     What is the largest prime factor of the number 600851475143?
+ *
+ * Author:      Sierikov Artem  (https://github.com/ArtemSer)
+ */
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem_003 {
+    private static List<Long> primeFactors(long number) {
+        long userNumber = number;
+        List<Long> factors = new ArrayList<>();
+        for (long primer = 2; primer <= userNumber; primer++) {
+            while (userNumber % primer == 0) {
+                factors.add(primer);
+                userNumber /= primer;
+            }
+        }
+        return factors;
+    }
+
+    public static void main(String[] args) {
+        long userNumber = 600851475143L;
+        System.out.println("Primefactor of 600851475143");
+        for (Long longer : primeFactors(userNumber)) {
+            System.out.println(longer);
+        }
+    }
 }
